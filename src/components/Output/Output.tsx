ffef16f038bf2b4069196eb6f73535d2c22b4ea9
Kalle Sundell <sundellkalle@gmail.com>
2024-10-09 14:59:50 +0300

    implement loading indicator (dynamic text)

--- src/components/Output/Output.tsx
+++ src/components/Output/Output.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { Riple } from "react-loading-indicators"
 
 type OutputProps = {
@@ -11,7 +12,18 @@ const Output = ({
   setUri,
   isLoading
 }: OutputProps) => {
-  
+  const dotStates = [' . ', ' . . ', ' . . . ']
+  const [dots, setDots] = useState<string>(dotStates[0])
+
+  useEffect(() => {
+    let index = 1
+    const interval = setInterval(() => {
+      setDots(dotStates[index])
+      index = (index + 1) % dotStates.length
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   const onDownload = () => {
     // API...
     setUri(undefined)
@@ -33,6 +45,7 @@ const Output = ({
     return (
       <div className="output" id="loading" data-testid="load-wrapper">
         <Riple color="#3f3f3f" size="large" />
+        <span>Your robot is being generated{dots}</span>
       </div>
     )
   }
